a874afaa1000c4b7dc09682b2306fafd529fe32a
BernabeJudicael <bernabejudicael02@gmail.com>
2025-09-20 13:38:58 +0200

    Votre message

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 
 // Configuration CORS complète
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:  ['https://scanflow-liard.vercel.app/', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
